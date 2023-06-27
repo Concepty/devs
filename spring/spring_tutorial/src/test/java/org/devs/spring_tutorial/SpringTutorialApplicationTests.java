e5a87cbb76f09207a18bcb896ecfe92ec9724b23
Concepty <Concepty@gmail.com>
2023-06-28 07:25:03 +0900

    SpringBoot tutorial - Error in Dependency

---
+++ spring/spring_tutorial/src/test/java/org/devs/spring_tutorial/SpringTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devs.spring_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
