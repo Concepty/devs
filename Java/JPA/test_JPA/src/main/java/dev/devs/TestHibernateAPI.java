e474c87dd19447347ac3438e1e927ae3b19b59ff
Concepty <hwansu@KimHwansus-MacBook-Pro.local>
2023-07-06 22:08:57 +0900

    create table if not exist is done, sequentially, using @GeneratedValue in @Id is done. Commited before refactoring classes.

--- Java/JPA/test_JPA/src/main/java/dev/devs/TestHibernateAPI.java
+++ Java/JPA/test_JPA/src/main/java/dev/devs/TestHibernateAPI.java
@@ -3,9 +3,9 @@ package dev.devs;
 // TODO: search why jakarta is imported instead of javax even if I'm using openjdk@11.
 // TODO: GPT said sth about this but couldn't be trusted.
 import jakarta.persistence.*;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+import org.hibernate.*;
 import org.hibernate.Version;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 
@@ -41,12 +41,14 @@ public class TestHibernateAPI {
         // REF: https://stackoverflow.com/questions/53382161/message-could-not-read-a-hi-value-you-need-to-populate-the-table-hibernate
         @Id
 //        @GeneratedValue(strategy=GenerationType.SEQUENCE)
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Integer id;
         @Column(name="name")
         private String name;
         // TODO why long? not int?
 
         public Hibernate1() {}
+        public Hibernate1(String name) { this.name = name; }
         /* Getters */
 
         public String getName() {
@@ -83,22 +85,80 @@ public class TestHibernateAPI {
         prop.setProperty("hibernate.connection.password", "1234");
         prop.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
         prop.setProperty("connection.pool_size", "1");
+        prop.setProperty("hibernate.hbm2ddl.auto", "update");
         prop.setProperty("show_sql", "true");
         // TODO can I set mapping in property or related xml?
 
 
+        Configuration configuration = new Configuration().addProperties(prop).addAnnotatedClass(Hibernate1.class);
+        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties());
+        SessionFactory sessionFactory = configuration.buildSessionFactory(builder.build());
+
+
+
+
         // INFO: addAnnotatedClass was the reason of no persister error
-        SessionFactory sessionFactory = new Configuration().addProperties(prop).addAnnotatedClass(Hibernate1.class).buildSessionFactory();
         Session session = sessionFactory.openSession();
         session.beginTransaction();
-        hibernate1 = new Hibernate1();
 
-        hibernate1.setName(name);
-        hibernate1.setId(5);
+        // Insert methods: persist, save, merge
+        Hibernate1 test_merge, test_save, test_persist;
 
-        session.merge(hibernate1);
+        test_merge = new Hibernate1("merge");
+        test_save = new Hibernate1("save");
+        test_persist = new Hibernate1("persist");
+
+        session.merge(test_merge);
+        session.save(test_save);
+        session.persist(test_persist);
         session.getTransaction().commit();
+
         session.close();
+        session = sessionFactory.openSession();
+        session.beginTransaction();
+
+
+        // Read methods: load, get
+
+        // TODO: what is entity name parameter of get
+        Hibernate1 test_record = (Hibernate1)session.get(Hibernate1.class, 1);
+        System.out.println("id_1:  " + test_record.getName());
+        test_record = null;
+
+        // TODO: try to read before commit
+
+
+        // Update methods update
+        test_merge.setName("merge updated");
+//        session.update(test_merge);
+
+        // Not allowed twice!
+        session.getTransaction().commit();
+
+        // Read again
+        test_record = (Hibernate1)session.get(Hibernate1.class, 1);
+        System.out.println("id_1:  " + test_record.getName());
+        test_record = null;
+
+
+
+
+        // Delete methods delete
+
+
+
+
+
+
+
+
+
+
+
+
+
+    session.close();
 
 
 
@@ -114,6 +174,9 @@ public class TestHibernateAPI {
 *   where: save or merge
 *   reason: didn't addAnnotatedClass() to hibernate Configuration. Obviously @Entity wasn't enough
 *   solution: self-explanatory
+*   how solved: Cuz Hibernate didn't find persister (dev.devs.TestHibernateAPI$Hibernate1), there might be problem setting or finding.
+*               First action was to check valid tutorial to follow because I'm new to this.
+*               And noticed that I missed addAnnotatedClass.
 *   TODO: check if addAnnotatedClass() is specified in JPA or not.
 *
 *   'test_hibernate.test_crud_seq' doesn't exist
@@ -122,12 +185,21 @@ public class TestHibernateAPI {
 *   reason: (gpt) Hibernate relies on a database sequence to generate the unique identifiers for the entities
 *   reason: (gpt, guess) Hibernate need configured table to work properly. Maybe the schema of table should be configured in Hibernate.
 *   solution: (not tested) INSERT INTO <schema_name>.hibernate_sequence (next_val) VALUES (0);
+*   How solved: test_hibernate.test_crud_seq table come from nowhere. I suspected that my database setting is not proper for hibernate.
+*               Of course there are proper way to configure my database. But I rather let hibernate configure itself. So I decided to
+*               try create table with Hibernate.
 *
 *
 *   No default constructor for entity : dev.devs.TestHibernateAPI$Hibernate1
 *   where: merge
 *   solution: change Hibernate1 to static to free it from context binding to its enclosing class.
 *   reason: maybe TestHibernateAPI$Hibernate1 is not constructable without TestHibernateAPI instance.
+*   how solved: eas-y
 *
+*   The given object has a null identifier: dev.devs.TestHibernateAPI$Hibernate1
+*   where: update
+*   solution: ?
+*   reason: ?
+*   how solved: search what is identifier in @Entity object
 *
 * */
