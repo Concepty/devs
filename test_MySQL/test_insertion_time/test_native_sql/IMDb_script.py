1c305bb82c238becf6687cacc822ea0fceb5adac
Concepty <khs1903b@gmail.com>
2023-08-21 01:04:44 +0900

    script generator refactored

--- test_MySQL/test_insertion_time/test_native_sql/IMDb_script.py
+++ test_MySQL/test_insertion_time/test_native_sql/IMDb_script.py
@@ -3,34 +3,43 @@
 import mysql.connector
 import time
 import sys
+from optparse import OptionParser
 
-if len(sys.argv) == 3:
-    INSERT_UNIT = int(sys.argv[1])
-else:
-    INSERT_UNIT = 100
+CONFIG = {}
 
+parser = OptionParser()
+parser.add_option('--unit', '-u', help = 'insert unit',
+                    dest = 'unit', type = 'int', default = 100)
+parser.add_option('--JPA', help = 'if set, generate a script for JPA',
+                   dest = 'JPA', action = 'store_true', default = False)
 
-def generate_sql_queries(DML_only = False):
+options, args = parser.parse_args()
+CONFIG['INSERT UNIT'] = options.unit
+CONFIG['JPA'] = options.JPA
+
+def generate_sql_queries(unit: int, for_jpa: bool = False):
     out = []
     with open('title.ratings.tsv', 'rt') as f_in:
         f_in.readline() # ignore the first line
-        if not DML_only:
+        if not for_jpa:
             out.append('drop table if exists Title_Rating;')
             out.append('CREATE TABLE Title_Rating (tconst VARCHAR(255) NOT NULL, averageRating DOUBLE PRECISION, numVotes INT, PRIMARY KEY (tconst));')
         while True:
             buff = []
-            for cursor in range(INSERT_UNIT):
+            for _ in range(unit):
                 line = f_in.readline()
                 if not line: break
                 id, rating, votes = line.rstrip().split('\t')
                 buff.append(f'("{id}", {rating}, {votes})')
             if not buff: break
             out.append('INSERT INTO Title_Rating (tconst, averageRating, numVotes) VALUES ' + ', '.join(buff) + ';')
-    with open(f'insert_by_{INSERT_UNIT}.sql', 'wt') as f_out:
+    file_name = f'insert_by_{unit}.sql' if not for_jpa else f'insert_by_{unit}_with_jpa.sql'
+    with open(file_name, 'wt') as f_out:
         for line in out:
             f_out.write(line + '\n')
     return out
 
+# test functions
 def insert_by_queries(queries: list):
     cnx = mysql.connector.connect(user = 'test', password = '1234', host = 'localhost', database = 'test_IMDb')
 
@@ -47,7 +56,7 @@ def insert_by_queries(queries: list):
 
     end_time = time.time()
 
-    print(f'time spent(INSERT unit: {INSERT_UNIT}): {end_time - start_time}')
+    print(f'time spent(INSERT unit: {CONFIG["INSERT UNIT"]}): {end_time - start_time}')
 
 def insert_by_file():
     cnx = mysql.connector.connect(user = 'test', password = '1234', host = 'localhost', database = 'test_IMDb')
@@ -55,7 +64,7 @@ def insert_by_file():
 
     start_time = time.time()
 
-    with open(f'insert_by_{INSERT_UNIT}.sql', 'rt') as f:
+    with open(f'insert_by_{CONFIG["INSERT UNIT"]}.sql', 'rt') as f:
         while True:
             line = f.readline()
             if not line: break
@@ -64,16 +73,11 @@ def insert_by_file():
     
     end_time = time.time()
 
-    print(f'time spent(INSERT unit: {INSERT_UNIT}): {end_time - start_time}')
 
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 3 and sys.argv[2] == 'true':
-        generate_sql_queries(True)
-    else:
-        queries = generate_sql_queries()
-
+    generate_sql_queries(CONFIG['INSERT UNIT'])
 
     """
     3 -> 60.7 secs
